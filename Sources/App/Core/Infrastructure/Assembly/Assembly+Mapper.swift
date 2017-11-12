52bf309adbf989ccc73813a04b64f9a678bef4e6
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-12 13:28:46 +0100

    Wire dependencies

--- Sources/App/Core/Infrastructure/Assembly/Assembly+Mapper.swift
+++ Sources/App/Core/Infrastructure/Assembly/Assembly+Mapper.swift
@@ -1,9 +1,38 @@
 import CoreService
 
+// MARK: - Game
+
 extension Assembly {
+  // MARK: - Domain
+  
+  var gameMapper: GameMapper {
+    return GameMapper(
+      manufacturerMapper: manufacturerMapper,
+      gameConsoleMapper: gameConsoleMapper,
+      genreMapper: genreMapper
+    )
+  }
+  
+  private var manufacturerMapper: ManufacturerMapper {
+    return ManufacturerMapper()
+  }
+  
+  private var genreMapper: GenreMapper {
+    return GenreMapper()
+  }
+}
+
+// MARK: - Game console
+
+extension Assembly {
+  
+  // MARK: - View
+  
   var gameConsoleViewMapper: GameConsoleViewMapper {
     return GameConsoleViewMapper()
   }
+  
+  // MARK: - Domain
   
   var gameConsoleMapper: GameConsoleMapper {
     return GameConsoleMapper()
