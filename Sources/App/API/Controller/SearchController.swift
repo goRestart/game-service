f032e8bda46cc83c98dbe3d29cac144fd902e42d
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-12 12:46:16 +0100

    Register search endpoint and pass information into the controller

---
+++ Sources/App/API/Controller/SearchController.swift
@@ -0,0 +1,8 @@
+import HTTP
+
+struct SearchController {
+  
+  func find(with request: Request) throws -> ResponseRepresentable {
+    return "ok"
+  }
+}
--- Sources/App/Core/Infrastructure/Assembly/Assembly+Controller.swift
+++ Sources/App/Core/Infrastructure/Assembly/Assembly+Controller.swift
@@ -3,8 +3,13 @@ import CoreService
 extension Assembly {
   var gameRouteCollection: GameRouteCollection {
     return GameRouteCollection(
+      searchController: searchController,
       gameConsoleController: gameConsoleController
     )
+  }
+  
+  private var searchController: SearchController {
+    return SearchController()
   }
   
   private var gameConsoleController: GameConsoleController {
