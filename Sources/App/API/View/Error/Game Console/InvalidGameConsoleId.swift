5f58800543db986bae1552314272f512d10c733b
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-17 19:18:36 +0100

    Update response for invalid game console error

--- Sources/App/API/View/Error/Game Console/InvalidGameConsoleId.swift
+++ Sources/App/API/View/Error/Game Console/InvalidGameConsoleId.swift
@@ -3,8 +3,7 @@ import HTTP
 extension Response {
   static let invalidGameConsoleId = try! Response.error(
     status: .notFound,
-    message: "Invalid game id",
-    identifier: "game.error.invalid_game_id"
+    message: "Invalid game console id",
+    identifier: "game.error.invalid_game_console_id"
   )
 }
-
